fb7c341e548ea7a395ec47c02a03c6ae400694a7
hbhandal80 <hbhandal80@hotmail.com>
2021-04-15 02:48:03 +0000

    Create a function

---
+++ even.js
@@ -0,0 +1,18 @@
+// Is Even function
+const isEven  = function (num) {
+  return num % 2 === 0;
+}
+
+const tenIsEven = isEven(10);
+const elevenIsEven = isEven(11);
+
+console.log(tenIsEven);
+console.log(elevenIsEven);
+
+// Is Even 2
+const isEven  = function (num) {
+  return num % 2 === 0;
+}
+
+console.log(isEven(10));
+console.log(isEven(11));
